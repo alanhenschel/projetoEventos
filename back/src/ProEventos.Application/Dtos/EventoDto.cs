d83ce2afd0371cc2a6ec5717e8426204dc5913d0
alan <alan_henschel@hotmail.com>
2021-09-12 23:49:25 -0300

    data annotation on EventoDTO

--- back/src/ProEventos.Application/Dtos/EventoDto.cs
+++ back/src/ProEventos.Application/Dtos/EventoDto.cs
@@ -1,16 +1,20 @@
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace ProEventos.Application.Dtos
 {
     public class EventoDto
     {
+        [Required]
         public int Id { get; set; }
         public string Local { get; set; }
         public string DataEvento { get; set; }
+        [Required(ErrorMessage = "O campo {0} é obrigatório.")]
         public string Tema { get; set; }
         public int QtdPessoas { get; set; }
         public string ImagemUrl { get; set; }
         public string Telefone { get; set; }
+        [EmailAddress]
         public string Email { get; set; }
         
         public IEnumerable<LoteDto> Lotes { get; set; }
