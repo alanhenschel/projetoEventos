674c2dadca8fb5262f91c119317e0f4b24f6254e
Alan Henschel Costa <alan.costa@meta.com.br>
2022-06-06 09:18:01 -0300

    interface User

---
+++ back/src/ProEventos.Application/Contratos/IUser.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProEventos.Application.Contratos
+{
+    public interface IUser
+    {
+        Task<bool> UserExists(string username);
+        Task<UserUpdateDTO> GerUsersByUsernameAsync(string username);
+        Task<SignInResult> CheckUserPasswordAsync(UserUpdateDTO user, string password);
+    }
+}
